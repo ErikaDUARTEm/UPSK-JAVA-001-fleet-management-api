d25e237048a0de6ba0aa3e0d7d38b27ba4cf9800
Erika Duarte <erika.duarte.mora@gmail.com>
2023-10-10 15:37:44 -0500

    Configuracion de spring boot

---
+++ server/fleetManagement/fleetManagement/src/main/java/com/api/fleetManagement/FleetManagementApplication.java
@@ -0,0 +1,14 @@
+package com.api.fleetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FleetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementApplication.class, args);
+	}
+
+}
