4ef615f9df64d97ba3c0c734de9b33e286bfdf5a
Erika Duarte <erika.duarte.mora@gmail.com>
2023-10-10 20:27:34 -0500

    variables de entorno y dependencia para que spring acceda a las variables de entorno

--- server/fleetManagement/fleetManagement/src/main/java/com/api/fleetManagement/FleetManagementApplication.java
+++ server/fleetManagement/fleetManagement/src/main/java/com/api/fleetManagement/FleetManagementApplication.java
@@ -1,12 +1,14 @@
 package com.api.fleetManagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @SpringBootApplication
 public class FleetManagementApplication {
-
+	Dotenv dotenv = Dotenv.configure().load();
 	public static void main(String[] args) {
 		SpringApplication.run(FleetManagementApplication.class, args);
 	}
